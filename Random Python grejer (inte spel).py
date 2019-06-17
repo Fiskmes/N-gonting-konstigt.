ffc0c8c6aea4e22ab8bfc2cacb1c4bd83332101a
Fiskmes <51903664+Fiskmes@users.noreply.github.com>
2019-06-17 09:24:54 +0200

    Add files via upload

---
+++ Random Python grejer (inte spel).py
@@ -0,0 +1,40 @@
+namn = input("Vad heter du? ") 
+
+ålder = int(input("Hur gammal är du? "))
+if ålder > 18:
+    print("Du är äldre än 18 ")
+
+elif ålder == 16:
+    print(namn, " du är 4 * 4 år gammal!")
+else:
+    print("Du är yngre än 18 ")
+
+
+for w in range(0):
+        print ("Hello World!")
+Hello_World = input("Hello")
+
+
+
+
+
+
+
+
+
+
+namn = input("Vad heter du? ") 
+
+ålder = int(input("Hur gammal är du? "))
+if ålder > 18:
+    print("Du är äldre än 18 ")
+
+elif ålder == 16:
+    print(namn, " du är 4 * 4 år gammal!")
+else:
+    print("Du är yngre än 18 ")
+
+
+for w in range(0):
+        print ("Hello World!")
+Hello_World = input("Hello")
